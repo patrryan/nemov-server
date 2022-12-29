e019ac7d856c78d04391535170efd2a9fdca6a08
이주형 <ezennt@ezennts-MacBook-Air.local>
2022-12-29 11:36:42 +0900

    [FEATURE] product API

---
+++ src/apis/products/interfaces/products-service.interface.ts
@@ -0,0 +1,29 @@
+import { CreateProductInput } from '../dto/create-product.input';
+import { UpdateProductInput } from '../dto/update-product.input';
+import { Product } from '../entities/product.entity';
+
+
+export interface IProductsServiceCreate {
+  createProductInput: CreateProductInput;
+}
+
+export interface IProductsServiceFindOne {
+  productId: string;
+}
+
+export interface IProductsServiceUpdate {
+  product: Product;
+  updateProductInput: UpdateProductInput;
+}
+
+export interface IProductsServiceCheckSoldout {
+  product: Product;
+}
+
+export interface IProductsServiceDelete {
+  productId: string;
+}
+
+export interface IProductsServicewithDeleted {
+  productId: string;
+}
---
+++ src/apis/products/products.resolver.ts
@@ -0,0 +1,61 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateProductInput } from './dto/create-product.input';
+import { UpdateProductInput } from './dto/update-product.input';
+import { Product } from './entities/product.entity';
+import { ProductsService } from './products.service';
+
+@Resolver()
+export class ProductsResolver {
+  constructor(
+    private readonly productsService: ProductsService, //
+  ) {}
+
+  ///-----------------------------///
+
+  @Query(() => [Product])
+  fetchProducts(): Promise<Product[]> {
+    return this.productsService.findAll();
+  }
+
+  ///-----------------------------///
+
+  @Query(() => Product)
+  fetchProduct(
+    @Args('productId') productId: string, //
+  ): Promise<Product> {
+    return this.productsService.findOne({ productId });
+  }
+
+  ///-----------------------------///
+
+  @Mutation(() => Product)
+  createProduct(
+    @Args('createProductInput')
+    createProductInput: CreateProductInput,
+  ): Promise<Product> {
+    return this.productsService.create({ createProductInput });
+  }
+
+  ///-----------------------------///
+
+  @Mutation(() => Product)
+  async updateProduct(
+    @Args('productId') productId: string,
+    @Args('updateProductInput') updateProductInput: UpdateProductInput,
+  ): Promise<Product> {
+    const product = await this.productsService.findOne({ productId });
+
+    return this.productsService.update({ product, updateProductInput });
+  }
+
+  ///-----------------------------///
+
+  @Mutation(() => Boolean)
+  deleteProduct(
+    @Args('productId') productId: string, //
+  ): Promise<boolean> {
+    return this.productsService.delete({ productId });
+  }
+
+  ///-----------------------------///
+}
