7ea520ecc6602a950ab64509c187707b1f221208
YUNRAYUN <rayun.yun95@gmail.com>
2022-12-29 19:00:35 +0900

    [FEATURE] phone API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
@@ -13,6 +13,9 @@ import { FilesModule } from './apis/files/file.module';
 import { AdminModule } from './apis/admin/admin.module';
 import { JwtAdminAccessStrategy } from './commons/auth/jwt-admin-access-strategy';
 import { ProductsModule } from './apis/products/products.module';
+import { PhoneModule } from './apis/phone/phone.module';
+import { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -34,11 +37,17 @@ import { ProductsModule } from './apis/products/products.module';
       logging: true,
       // retryAttempts: 30,
     }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      url: 'reids://my-redis:6379',
+      isGlobal: true,
+    }),
     FilesModule,
     AdminModule,
     AuthModule,
     UsersModule,
     ProductsModule,
+    PhoneModule,
   ],
   providers: [
     AppService, //
