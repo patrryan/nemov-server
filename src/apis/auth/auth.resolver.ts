116ebb66a823cc238381518a8b9a10650a855687
이주형 <ezennt@ezennts-MacBook-Air.local>
2023-01-01 22:13:35 +0900

    [FEATURE] logout api

--- src/apis/auth/auth.resolver.ts
+++ src/apis/auth/auth.resolver.ts
@@ -1,16 +1,29 @@
-import { UnprocessableEntityException, UseGuards } from '@nestjs/common';
+import {
+  UnprocessableEntityException,
+  UseGuards,
+  CACHE_MANAGER,
+  Inject,
+} from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { IContext } from 'src/commons/types/context';
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 import * as bcrypt from 'bcrypt';
-import { GqlAuthRefreshGuard } from 'src/commons/auth/gql-auth.guard';
+import {
+  GqlAuthAccessGuard,
+  GqlAuthRefreshGuard,
+} from 'src/commons/auth/gql-auth.guard';
+import { Cache } from 'cache-manager';
+import * as jwt from 'jsonwebtoken';
 
 @Resolver()
 export class AuthResolver {
   constructor(
     private readonly usersService: UsersService, //
     private readonly authService: AuthService,
+
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
   ) {}
 
   @Mutation(() => String)
@@ -34,9 +47,63 @@ export class AuthResolver {
 
   @UseGuards(GqlAuthRefreshGuard)
   @Mutation(() => String)
-  restoreAccessToken(
-    @Context() context: IContext, //
-  ): string {
+  restoreAccessToken(@Context() context: IContext): string {
     return this.authService.getAccessToken({ id: context.req.user.id });
+  }
+  //--------------------------------------
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => String)
+  async logout(@Context() context: IContext) {
+    const accessToken = context.req.headers.authorization.replace(
+      'Bearer ',
+      '',
+    );
+    // console.log('==============', accessToken);
+    const refreshToken = context.req.headers.cookie.replace(
+      'refreshToken=',
+      '',
+    );
+
+    // console.log('==============', refreshToken);
+
+    const verifiedAccess = jwt.verify(accessToken, process.env.JWT_ACCESS_KEY);
+
+    const verifiedRefresh = jwt.verify(
+      refreshToken,
+      process.env.JWT_REFRESH_KEY,
+    );
+
+    const current = new Date().getTime();
+
+    const ttlOfAccess = Math.trunc(
+      (verifiedAccess['exp'] * 1000 - current) / 1000,
+    );
+
+    const ttlOfRefresh = Math.trunc(
+      (verifiedRefresh['exp'] * 1000 - current) / 1000,
+    );
+
+    try {
+      verifiedAccess;
+      await this.cacheManager.set(
+        // 캐시매니저에 저장
+        `accessToken = ${accessToken}`,
+        'accessToken',
+        { ttl: ttlOfAccess },
+      );
+
+      verifiedRefresh;
+      await this.cacheManager.set(
+        // 캐시매니저 저장
+        `refreshToken = ${refreshToken}`,
+        'refreshToken',
+        { ttl: ttlOfRefresh },
+      );
+    } catch (error) {
+      throw new UnprocessableEntityException(error);
+    }
+
+    return '로그아웃 성공';
   }
 }
--- src/commons/auth/jwt-refresh.strategy.ts
+++ src/commons/auth/jwt-refresh.strategy.ts
@@ -1,21 +1,41 @@
+import { CACHE_MANAGER, Inject, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { Cache } from 'cache-manager';
 import { Strategy } from 'passport-jwt';
 
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
-  constructor() {
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+  ) {
     super({
       jwtFromRequest: (req) => {
+        console.log(req);
         const cookie = req.headers.cookie;
         const refreshToken = cookie.replace('refreshToken=', '');
         return refreshToken;
       },
       secretOrKey: process.env.JWT_REFRESH_KEY,
+      passReqToCallback: true,
     });
   }
 
-  validate(payload) {
+  async validate(req, payload) {
+    console.log('===============', req.headers.cookie);
+
+    const refreshToken = req.headers.cookie.replace('refreshToken=', '');
+    const cheekRefresh = await this.cacheManager.get(
+      `refreshToken = ${refreshToken}`,
+    );
+
+    if (cheekRefresh) {
+      throw new UnauthorizedException('로그아웃된 토큰입니다.');
+    }
+
     return {
-      id: payload.id,
+      email: payload.email,
+      id: payload.sub,
+      exp: payload.exp,
     };
   }
 }
