ec1ae58e9b59ce06be9b875d9548f3bc33201949
patrryan <patangmond329@gmail.com>
2023-01-07 17:32:57 +0900

    [MODIFY] auth service

--- src/commons/types/context.ts
+++ src/commons/types/context.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 
 export interface IAuthUserItem {
-  email: string;
   id: string;
 }
 
 export interface IAuthUser {
   user?: IAuthUserItem;
 }
+
 export interface IContext {
   req: Request & IAuthUser;
   res: Response;
