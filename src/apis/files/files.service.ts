cfedb0c31cfa80bfaa5099fb438ff5a3c69960c4
이주형 <ezennt@ezennts-MacBook-Air.local>
2023-01-09 20:12:39 +0900

    [MODIFY] files api

--- src/apis/files/files.service.ts
+++ src/apis/files/files.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { IFilesServiceUpload } from './interfaces/files-service.interface';
 import { Storage } from '@google-cloud/storage';
 import { v4 as uuidv4 } from 'uuid';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class FilesService {
@@ -9,18 +10,22 @@ export class FilesService {
     const uuid = uuidv4();
     const storage = new Storage({
       projectId: process.env.GCP_PROJECTID,
-      keyFilename: process.env.GCP_STORAGE, //암호파일 이름
+      keyFilename: process.env.GCP_STORAGE,
     }).bucket(process.env.GCP_BUCKET);
     const result = new Promise<string>((resolve, reject) => {
       file
         .createReadStream()
-        .pipe(storage.file(`${uuid}${file.filename}`).createWriteStream({}))
-
+        .pipe(sharp().toFormat('webp').webp({ quality: 80, force: true }))
+        .pipe(
+          storage
+            .file(`${uuid}${file.filename.split('.')[0]}.webp`)
+            .createWriteStream({}),
+        )
         .on('finish', () =>
           resolve(
             'https://storage.googleapis.com/' +
               process.env.GCP_BUCKET +
-              `/${uuid}${file.filename}`,
+              `/${uuid}${file.filename.split('.')[0]}.webp`,
           ),
         )
         .on('error', () => reject('실패했습니다.'));
